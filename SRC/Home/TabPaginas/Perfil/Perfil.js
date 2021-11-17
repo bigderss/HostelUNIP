7b19941d88b8f83bbf8beccd595b7d7e1fefed1a
Carlos <67762258+bigderss@users.noreply.github.com>
2021-11-17 16:24:30 -0300

    Add files via upload

--- SRC/Home/TabPaginas/Perfil/Perfil.js
+++ SRC/Home/TabPaginas/Perfil/Perfil.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Container} from '../Perfil/Styles'
 import { TouchableOpacity } from "react-native";
+import { Avatar, PricingCard } from 'react-native-elements';
 
 
 
@@ -15,7 +16,30 @@ const Perfil =()=>{
 
     return(
         <Container>
-           
+             <Avatar
+    size="xlarge"
+    title="CE"
+  rounded
+  source={{
+    uri:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+  }}
+/>
+            <PricingCard
+          
+          color="#4f9deb"
+          containerStyle={{marginTop:50}}
+         title="Carlos Eduardo "
+         price=""
+         info={['22 Anos', 'RG:52.264.***.-*', 'CPF:463.443.***-***', 'Email:Carlos.*****@aluno.unip.br ']}
+       button={{ title: 'Editar', icon: 'edit' }}
+
+
+
+
+/>
+              
+
                 
 
         </Container>
